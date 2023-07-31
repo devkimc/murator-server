41886c45b0945dfaf27b92c2a56072cae522687d
WooSeok Kim <kws60000@naver.com>
2023-07-31 13:15:53 +0900

    Initial commit

---
+++ src/main/java/com/vvs/murator/MuratorApplication.java
@@ -0,0 +1,13 @@
+package com.vvs.murator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vvs/murator/MuratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vvs.murator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
